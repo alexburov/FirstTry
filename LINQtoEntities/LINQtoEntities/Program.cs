d5d5d1c6922777ea6f4c0c75cf8aeb205953a3a9
Alex Burov <alex.burov@outlook.com>
2013-08-08 20:10:44 -0700

    LINQtoEntities project added.

---
+++ LINQtoEntities/LINQtoEntities/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LINQtoEntities
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Database1Entities3 de = new Database1Entities3();
+
+      var customers = from c in de.Customers
+                      where c.Orders.Any(o => o.OrderAmount > 150)
+                      select c;
+
+
+      foreach (var c in customers)
+      {
+        Console.WriteLine(c.LastName + " Orders " + c.Orders.Count().ToString());
+
+
+
+        foreach (var o in c.Orders)
+        {
+          Console.WriteLine(o.OrderDate.ToString() + " - " + o.OrderAmount.ToString());
+        }
+      }
+
+
+      Console.ReadLine();
+
+
+
+
+
+    }
+  }
+}
