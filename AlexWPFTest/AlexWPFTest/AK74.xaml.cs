0dca58c7407d74c9cebb56ad6786930dfa2a6c83
Alex Burov <alex.burov@outlook.com>
2013-08-08 20:03:24 -0700

    AlexWPFTest project added.

---
+++ AlexWPFTest/AlexWPFTest/AK74.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AlexWPFTest
+{
+  /// <summary>
+  /// Interaction logic for AK74.xaml
+  /// </summary>
+  public partial class AK74 : Window
+  {
+    public AK74()
+    {
+      InitializeComponent();
+    }
+  }
+}
