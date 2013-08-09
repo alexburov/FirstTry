39fe7cbbd94d195a3a1f8c8727048bb2de4f4b1b
Alex Burov <alex.burov@outlook.com>
2013-08-08 20:07:25 -0700

    Database project added.

---
+++ Database/dbo.Table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[CustumerID] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [Adress] NVARCHAR(50) NULL, 
+    [City] NVARCHAR(50) NULL, 
+    [State] NVARCHAR(20) NULL, 
+    [Zip] NCHAR(10) NULL, 
+    [Email] NVARCHAR(100) NULL, 
+    [CreditLimit] SMALLMONEY NOT NULL IDENTITY, 
+    [CustumerSince] SMALLDATETIME NOT NULL
+)
